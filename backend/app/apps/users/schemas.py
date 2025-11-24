76e67ed26c8b02ae65eaa79dd332c3771b95ee9a
Maksym <PeterParker@gmail.com>
2025-11-24 20:44:36 +0200

    save user

--- backend/app/apps/users/schemas.py
+++ backend/app/apps/users/schemas.py
@@ -1,18 +1,17 @@
-from pydantic import BaseModel, Field, EmailStr, ConfigDict
+from pydantic import BaseModel, EmailStr, ConfigDict
 
 
 class UserPasswordSchema(BaseModel):
     password: str
-
-
 class UserBaseFieldsSchema(BaseModel):
     email: EmailStr
     name: str
-
     model_config = ConfigDict(
         str_strip_whitespace=True
     )
 
-
 class RegisterUserSchema(UserPasswordSchema, UserBaseFieldsSchema):
     pass
+
+class SavedUserSchema(UserBaseFieldsSchema):
+    id: int
